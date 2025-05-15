d32a596220181182164e76f04ff72b6300764c60
ductiens <carina.ndt@gmail.com>
2025-05-15 09:51:39 +0700

    API Create

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,5 +1,6 @@
 import Joi from "joi";
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from "~/utils/validators";
+import { GET_DB } from "~/config/mongodb";
 
 // Define Collection-Table (name & schema)
 const BOARD_COLLECTION_NAME = "boards";
@@ -8,7 +9,7 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
 
-  // Lưu ý các item trong mảng columnOrderIds là ObjectId nên cần thêm pattern cho chuẩn nhé
+  // Lưu ý các item trong mảng columnOrderIds là ObjectId nên cần thêm pattern cho chuẩn
   columnOrderIds: Joi.array().items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)).default([]),
 
   createdAt: Joi.date().timestamp("javascript").default(Date.now),
@@ -16,7 +17,29 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false),
 });
 
+const createNew = async (data) => {
+  try {
+    const createBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data);
+    return createBoard;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const findOneById = async (id) => {
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
+      _id: id,
+    });
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 export const boardModel = {
   BOARD_COLLECTION_NAME,
   BOARD_COLLECTION_SCHEMA,
+  createNew,
+  findOneById,
 };
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-useless-catch */
 
 import { slugify } from "~/utils/formatters";
+import { boardModel } from "~/models/boardModel";
 
 const createNew = async (reqBody) => {
   try {
@@ -10,12 +11,16 @@ const createNew = async (reqBody) => {
     };
 
     //Gọi tới thằng Model để sử lý bản ghi newBoard trong database
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    //Lấy bản ghi board sau khi gọi (tùy mục đích dự án xem có cần bước này hay không)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
 
     //Làm thêm các sử lý logic khác với các Collection khác tùy đặc thù dự án
     //Bắn email, notification về cho admin khi có 1 board mới đc tạo
 
     //Trả về kết quả, trong Services luôn phải có return
-    return newBoard;
+    return getNewBoard;
   } catch (error) {
     throw error;
   }
