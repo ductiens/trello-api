b02a44fcddc921d5db3d5244a247ab2f1d83b7b7
ductiens <carina.ndt@gmail.com>
2025-05-19 16:02:01 +0700

    WHITELIST_DOMAINS.1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 export const WHITELIST_DOMAINS = [
   // "http://localhost:5173", //không cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường env.BUILD_MODE === "dev"
   //...vv ví dụ sau này sẽ deploy lên domain chính thức ...vv
-  "https://trello-web-omega-five.vercel.app/",
+  "https://trello-web-omega-five.vercel.app",
 ];
 
 export const BOARD_TYPES = {
